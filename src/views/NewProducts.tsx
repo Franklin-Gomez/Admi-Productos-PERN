02f49d1157dc3b1975f8288300da831a020f7741
Franklin-Gomez <franklin_043097@hotmail.com>
2024-07-29 13:48:45 -0500

    get dataform with function action and not state

--- src/views/NewProducts.tsx
+++ src/views/NewProducts.tsx
@@ -1,68 +1,83 @@
-import { Link , Form } from "react-router-dom"
+import { Link , Form, useActionData } from "react-router-dom"
 
 
 // funcion para procesar los datos
-export async function action() { 
-    console.log('...desde actions')
+export async function action({request}) { 
+
+    const data = Object.fromEntries( await  request.formData() )
+  
+    let error = ''
+
+    if( Object.values(data).includes('')) { 
+        error = 'Todos los campos son obligatorios'
+    }
+
+    if( error.length ) { 
+        return error
+    }
 
     return {} // siempre retornaremos algo
 }
 
 export default function NewProducts() {
-  return (
-    <>
-        <div className="flex justify-between">
-            <h2 className="text-4xl font-black text-slate-500"> Registrar Productos </h2>
 
-            <Link 
-                to='/'
-                className="rounded-md bg-indigo-600 p-3 text-sm font-bold text-white hover:bg-indigo-300 shadow-sm "
+    const error = useActionData()
+
+
+    return (
+        <>
+            <div className="flex justify-between">
+                <h2 className="text-4xl font-black text-slate-500"> Registrar Productos </h2>
+
+                <Link 
+                    to='/'
+                    className="rounded-md bg-indigo-600 p-3 text-sm font-bold text-white hover:bg-indigo-300 shadow-sm "
+                >
+                    Volver a Productos
+                </Link>
+
+            </div>
+
+            <Form
+                className="mt-10" 
+                method="POST"     
             >
-                Volver a Productos
-            </Link>
+            
+                <div className="mb-4">
+                    <label
+                        className="text-gray-800"
+                        htmlFor="name"
+                    >Nombre Producto:</label>
+                    <input 
+                        id="name"
+                        type="text"
+                        className="mt-2 block w-full p-3 bg-gray-50"
+                        placeholder="Nombre del Producto"
+                        name="name"
+                    />
+                </div>
 
-        </div>
+                <div className="mb-4">
+                    <label
+                        className="text-gray-800"
+                        htmlFor="price"
+                    >Precio:</label>
+                    <input 
+                        id="price"
+                        type="number"
+                        className="mt-2 block w-full p-3 bg-gray-50"
+                        placeholder="Precio Producto. ej. 200, 300"
+                        name="price"
+                    />
+                </div>
 
-        <Form
-            className="mt-10" 
-            method="POST"     
-        >
-        
-            <div className="mb-4">
-                <label
-                    className="text-gray-800"
-                    htmlFor="name"
-                >Nombre Producto:</label>
-                <input 
-                    id="name"
-                    type="text"
-                    className="mt-2 block w-full p-3 bg-gray-50"
-                    placeholder="Nombre del Producto"
-                    name="name"
+                <input
+                    type="submit"
+                    className="mt-5 w-full bg-indigo-600 p-2 text-white font-bold text-lg cursor-pointer rounded"
+                    value="Registrar Producto"
                 />
-            </div>
 
-            <div className="mb-4">
-                <label
-                    className="text-gray-800"
-                    htmlFor="price"
-                >Precio:</label>
-                <input 
-                    id="price"
-                    type="number"
-                    className="mt-2 block w-full p-3 bg-gray-50"
-                    placeholder="Precio Producto. ej. 200, 300"
-                    name="price"
-                />
-            </div>
-
-            <input
-                type="submit"
-                className="mt-5 w-full bg-indigo-600 p-2 text-white font-bold text-lg cursor-pointer rounded"
-                value="Registrar Producto"
-            />
-
-        </Form>
-    </>
-  )
+            </Form>
+        </>
+    )
 }

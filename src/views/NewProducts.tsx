dfcdea0b467fd4b6f9b4c930e0df1483fe39ddaa
Franklin-Gomez <franklin_043097@hotmail.com>
2024-07-29 11:35:32 -0500

    navetation between pages

--- src/views/NewProducts.tsx
+++ src/views/NewProducts.tsx
@@ -1,8 +1,20 @@
+import { Link } from "react-router-dom"
+
 
 export default function NewProducts() {
   return (
-    <div>
-        Productos nuevos
-    </div>
+    <>
+        <div className="flex justify-between">
+            <h2 className="text-4xl font-black text-slate-500"> Registrar Productos </h2>
+
+            <Link 
+                to='/'
+                className="rounded-md bg-indigo-600 p-3 text-sm font-bold text-white hover:bg-indigo-300 shadow-sm "
+            >
+                Volver a Productos
+            </Link>
+
+        </div>
+    </>
   )
 }
--- src/views/Products.tsx
+++ src/views/Products.tsx
@@ -1,7 +1,19 @@
+import { Link } from "react-router-dom"
+
 export default function Products() {
   return (
-    <div>
-        desde products
-    </div>
+    <>
+        <div className="flex justify-between">
+            <h2 className="text-4xl font-black text-slate-500">Productos</h2>
+
+            <Link 
+                to='productos/nuevos'
+                className="rounded-md bg-indigo-600 p-3 text-sm font-bold text-white hover:bg-indigo-300 shadow-sm "
+            >
+                Agregar Producto
+            </Link>
+
+        </div>
+    </>
   )
 }

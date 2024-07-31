0cbe940cf11a9ca0464e6202e5792f1f898df0cc
Franklin-Gomez <franklin_043097@hotmail.com>
2024-07-30 20:59:22 -0500

    API petition

--- src/services/ProductService.ts
+++ src/services/ProductService.ts
@@ -17,13 +17,11 @@ export const addProduct =  async ( data  : addProductPros) => {
 
         if( result.success) { 
 
-            const url = `${import.meta.env.VITE_API_URL}/api/products`;
+            const url = `${import.meta.env.VITE_API_URL}/api/products`
             const { data } = await axios.post(url , { 
                 name : result.output.name,
                 price : result.output.price
             })
-
-            console.log( data )
 
         } else { 
             throw new Error('Datos no validos')
